e057117c8c8195787f840d064b559d6656d24ac1
vibhutivadje <vibhutivadje@gmail.com>
2021-02-03 00:56:27 -0800

    Create 008. Weather Observation Station 3.sql

---
+++ SQL/1-Basic Select/008. Weather Observation Station 3.sql
@@ -0,0 +1,11 @@
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+-- # Score: 10
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID, 2) = 0;
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID%2 = 0;
