5ddde517ff217aa7913e733d391259f28156d326
vibhutivadje <vibhutivadje@gmail.com>
2021-03-19 16:58:44 -0700

    Create 001. Top Competitors.sql

---
+++ SQL/04. Basic Join/001. Top Competitors.sql
@@ -0,0 +1,14 @@
+SELECT 
+     h.hacker_id,
+     h.name
+FROM Submissions s
+INNER JOIN 
+    Challenges c ON s.challenge_id = c.challenge_id
+INNER JOIN 
+    Difficulty d ON c.difficulty_level = d.difficulty_level
+INNER JOIN 
+    Hackers h ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score AND c.difficulty_level = d.difficulty_level
+GROUP BY h.hacker_id, h.name 
+HAVING COUNT(s.hacker_id) > 1
+ORDER BY COUNT(s.hacker_id) DESC, s.hacker_id ASC
