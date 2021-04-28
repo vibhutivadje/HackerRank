0719546c06101859ce63aaaaa8157d352d713d28
vibhutivadje <vibhutivadje@gmail.com>
2021-04-27 22:45:21 -0700

    Update 003. Revising Aggregations - Averages.sql

--- SQL/04.Aggregation/003. Revising Aggregations - Averages.sql
+++ SQL/04.Aggregation/003. Revising Aggregations - Averages.sql
@@ -1,1 +1,8 @@
 
+-- # Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+-- # Score: 10
+
+
+SELECT AVG(Population)
+FROM City
+WHERE District = 'California';
